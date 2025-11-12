3786cb806e59d47dcb3f39582ec465d5156e05a4
deb.bunhor <deb.bunhor@sathapana.com.kh>
2025-11-12 22:13:37 +0700

    change heading

--- components/StoreHeading.js
+++ components/StoreHeading.js
@@ -1,14 +1,14 @@
-import PageTitle from '@/components/PageTitle'
+import PageTitle from "@/components/PageTitle";
 
 function StoreHeading() {
   return (
     <div className="">
-      <PageTitle text="Get Doggy Stickers!" />
+      <PageTitle text="Get Your Here!" />
       <p className="max-w-xl text-center px-2 mx-auto text-base text-gray-600">
-        Times are tough. Liven up your home with some cute Doggy Stickers. 🐶    
+        Times are tough. Liven up your home with some cute Doggy Stickers.
       </p>
     </div>
-  )
+  );
 }
 
-export default StoreHeading
+export default StoreHeading;
