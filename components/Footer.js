8c6886d4d7ee3a35d5a9ab2876aab655097fa422
deb.bunhor <deb.bunhor@sathapana.com.kh>
2025-11-12 15:19:33 +0700

    change footer

--- components/Footer.js
+++ components/Footer.js
@@ -4,16 +4,14 @@ import { faHeart } from "@fortawesome/free-solid-svg-icons";
 function Footer() {
   return (
     <footer className="py-4 flex justify-center font-primary items-center">
-      Built with{" "}
-      <FontAwesomeIcon icon={faHeart} className="w-5 text-red-600 mx-1" /> by{" "}
-      <a
-        href="https://twitter.com/deepwhitman"
-        target="_blank"
-        rel="noreferrer"
+      Thank You!{" "}
+      <FontAwesomeIcon icon={faHeart} className="w-5 text-red-600 mx-1" />{" "}
+      {/* <a
+        href="#"
+        target="#"
+        rel="#"
         className="text-palette-primary font-bold px-1"
-      >
-        Bilal Tahir
-      </a>
+      ></a> */}
     </footer>
   );
 }
